a0a2c0b6ed82ca40b7f3b1653827374bdb593912
Vadim Kharitonov <h.vadim@mail.ru>
2014-04-28 18:55:45 +0300

    ResultType added

---
+++ src/Bstu/Bundle/TestOrganizationBundle/Form/ResultType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bstu\Bundle\TestOrganizationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ResultType extends AbstractType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setRequired([
+            'question',
+            'real_answer',
+            'student_answer',
+        ])->setOptional([
+            'variants',
+        ])->setAllowedTypes([
+            'question' => 'string',
+            'real_answer' => 'string',
+            'student_answer' => 'string',
+            'variants' => ['null', 'array'],
+        ])->setDefaults([
+            'variants' => null,
+        ]);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['question'] = $options['question'];
+        $view->vars['real_answer'] = $options['real_answer'];
+        $view->vars['student_answer'] = $options['student_answer'];
+        $view->vars['variants'] = $options['variants'];
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'percent';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'result';
+    }
+}
