4f91ebbbb37231890205b5a6fd0f7772f46df276
Vadim Kharitonov <v_kharitonov@wargaming.net>
2014-05-02 09:59:16 +0300

    apc fix

--- web/app.php
+++ web/app.php
@@ -5,15 +5,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-$loader = new ApcClassLoader($_SERVER['USER'], $loader);
-$loader->register(true);
+//$loader = new ApcClassLoader($_SERVER['USER'], $loader);
+//$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
